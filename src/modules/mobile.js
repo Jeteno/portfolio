2244c265c02fed0256fb2a2eeab1b4c49cd42b57
Jeteno <macsim.sytov@yandex.ru>
2023-10-17 14:22:14 +0300

    code cleaning

--- src/modules/mobile.js
+++ src/modules/mobile.js
@@ -1,9 +1,6 @@
 export function mobile() {
-   // Получить все элементы, к которым хотите применить стиль :hover на мобильных устройствах
-// Получить элемент, к которому хотите применить изменение стилей
    const card = document.querySelector('.portfolio__card');
 
-   // Добавить обработчики событий прикосновения на мобильных устройствах
    card.addEventListener('touchstart', function() {
    this.classList.add('touched');
    });
